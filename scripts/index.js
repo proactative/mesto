b16735fe9c34c7605423af88a2c79d92a1ab6774
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2022-08-11 14:06:52 +0300

    fix: according to the review

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+/*for function openPopup*/
+let container = document.querySelector('.page__container');
+let profile = container.querySelector('.profile');
+let editButton = profile.querySelector('.profile__edit-button');
+
+/*for function formSubmitHandler*/
+let person = profile.querySelector('.profile__title');
+let job = profile.querySelector('.profile__text');
+let popup = document.querySelector('.popup');
+let form = popup.querySelector('.popup__container');
+let nameInput = form.querySelector('.popup__input_type_name');
+let jobInput = form.querySelector('.popup__input_type_job');
+
+/*for function closePopup*/
+let closeButton = popup.querySelector('.popup__close-link');
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+
+  nameInput.value = person.textContent;
+  jobInput.value = job.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  person.textContent = nameInput.value;
+  job.textContent = jobInput.value;
+
+  closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+form.addEventListener('submit', formSubmitHandler);
