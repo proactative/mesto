d35940776a8eeb1ce0b84da49400a14eda02a1a4
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2022-10-12 09:04:52 +0300

    fix: fix in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ const addFormValidation = new FormValidator(validationConfig, popupAddElement);
 addFormValidation.enableValidation();
 
 //zoom photo
-const popupWithImage = new PopupWithImage(popupZoomPhoto);
+const popupWithImage = new PopupWithImage('.popupZoomPhoto');
 popupWithImage.setEventListeners();
 
 //section
--- src/scripts/components/Section.js
+++ src/scripts/components/Section.js
@@ -3,6 +3,7 @@ export default class Section {
     this._items = items;
     this._renderer = renderer;
     this._container = containerSelector;
+    //this._container = document.querySelector(containerSelector);
   }
 
   renderItems() {
