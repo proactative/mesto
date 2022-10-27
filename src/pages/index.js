843b9fe4c46d0b04547f071202619617a992580e
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2022-10-27 11:53:05 +0300

    fix: folders refactoring

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import './index.css';
 
-import Card from '../scripts/components/Card.js';
-import FormValidator from '../scripts/components/FormValidator.js';
-import PopupWithForm from '../scripts/components/PopupWithForm.js';
-import PopupWithImage from '../scripts/components/PopupWithImage.js';
-import Section from '../scripts/components/Section.js';
-import UserInfo from '../scripts/components/UserInfo.js';
-import Api from '../scripts/components/Api.js';
-import PopupWithConfirmation from '../scripts/components/PopupWithConfirmation.js';
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import Section from '../components/Section.js';
+import UserInfo from '../components/UserInfo.js';
+import Api from '../components/Api.js';
+import PopupWithConfirmation from '../components/PopupWithConfirmation.js';
 
 import {
   profileEditButton,
@@ -25,7 +25,7 @@ import {
   popupEditProfile,
   avatarUpdateForm,
   popupEditAvatar
-} from '../scripts/utils/constants.js';
+} from '../utils/constants.js';
 
 //validation
 const editFormValidation = new FormValidator(validationConfig, popupEditProfile);
