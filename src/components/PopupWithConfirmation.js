1c8cefb9faa717c0b89174e5ae8b5e03501b9a6a
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2022-10-29 14:28:10 +0300

    feat: remove odd functions

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -13,10 +13,6 @@ export default class PopupWithConfirmation extends Popup {
     this._card = card;
   }
 
-  close() {
-    super.close();
-  }
-
   setEventListeners() {
     super.setEventListeners();
     this._form.addEventListener('submit', (evt) => {
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -19,10 +19,6 @@ export default class PopupWithForm extends Popup {
     return this._formValues;
   }
 
-  close() {
-    super.close();
-  }
-
   setInitialButtonText() {
     setTimeout(() => {
       this._button.textContent = this._button.getAttribute('initialTextContent');
